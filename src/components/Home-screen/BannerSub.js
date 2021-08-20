1b9817d6df2421fd67d0754a0bc653ac89b93a88
Syed Noman Ali <66224759+nomansd1@users.noreply.github.com>
2021-08-20 23:34:27 +0500

    work in progress

--- src/components/Home-screen/BannerSub.js
+++ src/components/Home-screen/BannerSub.js
@@ -9,31 +9,26 @@ import bottomImage from '../../assets/images/IMG_6118 1.png'
 function BannerSub(props) {
     return (
         <>
-        <div className={`section ${props.cname}`}>
-            <div className="sectionBx">
+        <div className="section">
+            <div className={`sectionBx ${props.cname}`}>
                 <div className="section_leftBx">
-                    <img src={subBanner} alt="" />
+                    <img src={props.banner} alt="" />
                 </div>
-                <div className="section_rightBx">
+                <div className={`section_rightBx`}>
                     <div>
                         <h1>
-                            Cucina Verace - <br/>
-                            True Italian Cuisine
+                            {props.mainh1}
                         </h1>
                         <p>
-                            La Stella represents more than just a restaurant concept.
-                            It is an immersive and truly authentic - verace - Italian
-                            dining experience comprised of immense passion and creativity.
+                            {props.para}
                         </p>
                     </div>
                 </div>
             </div>
             <div className="section_bottom">
-                <h1>La Stella - The Star amongst stars</h1>
+                <h1> {props.secBottomh1} </h1>
                 <p>
-                    Our name represents a promise to ourselves and to 
-                    our customers.  We set the benchmark for the finest 
-                    Italian dining experience in North Texas.
+                    {props.secBottomText}
                 </p>
             </div>
             <div className="vector">
@@ -41,7 +36,7 @@ function BannerSub(props) {
             </div>
         </div>
         <div className="bottom_image">
-            <img src={bottomImage} alt="" />
+            <img src={props.lowerImage} alt="" />
         </div>
         </>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,52 @@ import Navbar from '../components/Global-components/Navbar'
 import Banner from '../components/Home-screen/Banner'
 import BannerSub from '../components/Home-screen/BannerSub'
 
+// sub banner assets imports
+import subBanner from '../assets/images/IMG_5334 1.png'
+import bottomImage from '../assets/images/IMG_6118 1.png'
+import subBanner2 from '../assets/images/5383002 1.png'
+import bottomImage2 from '../assets/images/IMG_6113 1.png'
+
 
 function index() {
   return (
     <div>
       <Navbar/>
       <Banner/>
-      <BannerSub/>
       <BannerSub
+        banner={subBanner}
+        mainh1="
+          Cucina Verace - <br/>
+          True Italian Cuisine"
+        para="
+          La Stella represents more than just a restaurant concept.
+          It is an immersive and truly authentic - verace - Italian
+          dining experience comprised of immense passion and creativity."
+        secBottomh1="La Stella - The Star amongst stars"
+        secBottomText="
+          Our name represents a promise to ourselves and to 
+          our customers.  We set the benchmark for the finest 
+          Italian dining experience in North Texas."
+        lowerImage={bottomImage}
+      />
+      <BannerSub
+        banner={subBanner2}
+        mainh1="Le nostre origini - where we come from"
+        para="
+          Lorem ipsum dolor sit amet, consectetur adipiscing 
+          elit. Malesuada interdum aenean et volutpat commodo 
+          mattis nec gravida scelerisque."
+        secBottomh1="Fatto con amore - Prepared with love"
+        secBottomText="
+          Lorem ipsum dolor sit amet, consectetur adipiscing 
+          elit. Massa faucibus purus vitae eu donec etiam dui. 
+          Malesuada volutpat egestas faucibus aenean venenatis 
+          eu. Pellentesque lacus id nulla convallis at quis 
+          erat nulla id. Interdum accumsan pharetra sit fames 
+          tristique sit phasellus. In enim aliquet massa nunc 
+          ut quis aenean. Ut vehicula facilisi cum semper vestibulum 
+          sem accumsan sit."
+        lowerImage={bottomImage2}
         cname="reverse_row"
       />
     </div>
