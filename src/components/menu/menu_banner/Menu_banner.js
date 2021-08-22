2c5ce09760b98dcbae9d85d9c2f0e0db6ca5bbe5
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-08-22 23:15:36 +0500

    menu page updated

---
+++ src/components/menu/menu_banner/Menu_banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Menu_banner.css"
+function Menu_banner({banner_title}) {
+   
+    
+    return (
+        <div className="menu_banner" >
+            <div className="menu_banner_container">
+                   <h1>{banner_title}</h1>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Menu_banner
+
