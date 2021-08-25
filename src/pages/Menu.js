f79ff0abe4823292d11cf0ffd213ec5207b938ef
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-08-26 02:02:00 +0500

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react'
-// import "../components/menu/menu.css"
-// import Menu_banner from "../components/menu/menu_banner/Menu_banner"
-// import Menu_Sections from "../components/menu/menu_section/Menu_section"
-// import "../styles/Global-comp-styles/index.css"
-// import "../styles/Global-comp-styles/Navbar.css"
-// import AddressComponent from "../components/address_component/address_component"
-// import LepastaComponent from "../components/lepasta_component/Lepasta_Component"
-// function menu() {
-//     return (
-//         <div className="menu">
-              
-//               <Menu_banner banner_title="Menu" />
-=======
 import React from 'react'
 import "../components/menu/menu.css"
 import Menu_banner from "../components/menu/menu_banner/Menu_banner"
@@ -29,20 +14,14 @@ function menu() {
         <div className="menu">
               <Navbar />
               <Menu_banner banner_title="Menu" />
->>>>>>> d738f5a82d2f179b2f14964febc0ef345d758b5c
 
 //               <div className="menu_container">
                     
 //                     <Menu_Sections />
                    
-<<<<<<< HEAD
-//                     <AddressComponent />
-//                     <LepastaComponent />
-=======
                     <AddressComponent />
                     <LepastaComponent />
                     <Footer />
->>>>>>> d738f5a82d2f179b2f14964febc0ef345d758b5c
                 
 //               </div>
             
